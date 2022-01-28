ebab2136aa5eb2122441a88c4e9b1c9d0f6373e3
shldhee <shldhee@yahoo.co.kr>
2022-01-29 01:36:24 +0900

    feat: App Component 네비게이션 작업

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,57 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <nav class="nav">
+      <router-link
+        class="nav__link"
+        v-for="item in navList"
+        :key="item.id"
+        :to="item.path"
+      >
+        {{ item.name }}
+      </router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      navList: [
+        {
+          id: 'home',
+          path: '/',
+          name: 'Home',
+        },
+        {
+          id: 'card',
+          path: '/card',
+          name: 'Card',
+        },
+        {
+          id: 'form',
+          path: '/form',
+          name: 'Form',
+        },
+      ],
+    }
   },
 }
 </script>
 
-<style>
-#app {
+<style lang="scss" scoped>
+.nav {
+  &__link {
+    padding: 10px 11px;
+    color: #666;
+    font-size: 14px;
+  }
+
+  .router-link-active {
+    color: #ff7b30;
+  }
 }
 </style>
