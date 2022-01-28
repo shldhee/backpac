cea814734ae84e144bd6922c2c5cac0db6eed6da
shldhee <shldhee@yahoo.co.kr>
2022-01-29 02:24:09 +0900

    feat: Card 컴포넌트 스타일 수정
    - 말줄임표 추가
    - footer 영역 파트 없을 시 스타일 처리(v-if 사용 및 flex 사용)

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -15,9 +15,18 @@
       <h3 class="card__title">{{ title }}</h3>
       <p class="card__description">{{ description }}</p>
     </div>
-    <div class="card__footer">
-      <span class="card__rating"></span>
-      <p class="card__rating-text">{{ ratingText }}</p>
+    <div v-if="isShowFooter" class="card__footer">
+      <div v-if="rating" class="card__rating-wrapper">
+        <span
+          v-for="(rating, i) in computedRatingArray"
+          :key="i"
+          :class="{
+            'card__rating--active': rating,
+          }"
+          class="card__rating"
+        ></span>
+      </div>
+      <p v-if="ratingText" class="card__rating-text">{{ ratingText }}</p>
     </div>
   </div>
 </template>
@@ -31,26 +40,107 @@ export default {
     },
     label: {
       type: String,
-      default: 'Card Label',
+      default: 'Card Label Lorem ipsum dolor sit amet',
     },
     title: {
       type: String,
-      default: 'Card Title',
+      default: 'Card Title Lorem ipsum dolor sit amet',
     },
     description: {
       type: String,
-      default: 'Card Description',
+      default:
+        'Card Description Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet ',
     },
     rating: {
       type: Number,
-      default: 3,
+      default: 0,
     },
     ratingText: {
       type: String,
-      default: 'Rating Text',
+      default: '',
+    },
+  },
+  computed: {
+    computedRatingArray() {
+      return Array.from({ length: 5 }).map((_, i) => {
+        return i < this.rating
+      })
+    },
+    isShowFooter() {
+      return this.rating || this.ratingText
     },
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  width: 100%;
+  max-width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  &__header {
+  }
+  &__thumbnail {
+  }
+  &__body {
+    padding: 8px;
+    border-top: 1px solid #ccc;
+  }
+  &__label {
+    display: block;
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  &__title {
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  &__description {
+    width: 100%;
+    margin-top: 14px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+  &__footer {
+    height: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 8px;
+    border-top: 1px solid #ccc;
+  }
+  &__rating-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  &__rating {
+    background-color: #ccc;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    margin-right: 4px;
+    border-radius: 50%;
+    &--active {
+      background-color: #ff7b30;
+    }
+    &::last-child {
+      margin-right: 0;
+    }
+  }
+  &__rating-text {
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+}
+</style>
